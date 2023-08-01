e421fe0d9126cd02077d269082cd6235e5cbd9c5
tranvu2001 <tranvu20022001@gmail.com>
2023-08-01 23:43:17 +0700

    change CrossOrigin

--- src/main/java/demo/example/demo/controllers/BrandsController.java
+++ src/main/java/demo/example/demo/controllers/BrandsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import demo.example.demo.entity.Brands;
 import demo.example.demo.repository.BrandsRepository;
 
-@CrossOrigin(origins ="https://mini-hint-react-js.vercel.app/")
+@CrossOrigin(origins ="https://mini-hint-react-js.vercel.app")
 //@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/brands")
